7a84226fea286422e77214fe5222a38b733310f8
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2023-11-22 23:10:49 -0500

    example from init server, GET, POST

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ const app = new PlankServer();
 const PORT = 8080;
 
 
+app.router.get("/", () => {
+    console.log("get");
+});
+
+app.router.get("/about", () => {
+    console.log("get");
+});
+
+app.router.post("/form/{id:2}", (req, res) => {
+    console.log("POST");
+});
+
+
+
+console.log(app.router.routes)
+
+
+
+
+
 app.RunServer(PORT, () => {
     console.log(`Server listen in http://127.0.0.1:${PORT}`);
 });
